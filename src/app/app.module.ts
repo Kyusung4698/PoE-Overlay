6f53a8c5ed376df62272adc6bd8235f525917673
ITesserakt <potryas66@mail.ru>
2020-03-27 20:32:19 +0300

    Initial files for trading feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { BookmarkModule } from '@modules/bookmark/bookmark.module';
-import { CommandModule } from '@modules/command/command.module';
-import { EvaluateModule } from '@modules/evaluate/evaluate.module';
-import { MapModule } from '@modules/map/map.module';
-import { MiscModule } from '@modules/misc/misc.module';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { AppTranslationsLoader } from './app-translations.loader';
-import { AppComponent } from './app.component';
-import { LayoutModule } from './layout/layout.module';
-import { OverlayComponent } from './layout/page/overlay/overlay.component';
-import { UserSettingsComponent } from './layout/page/user-settings/user-settings.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BookmarkModule} from '@modules/bookmark/bookmark.module';
+import {CommandModule} from '@modules/command/command.module';
+import {EvaluateModule} from '@modules/evaluate/evaluate.module';
+import {MapModule} from '@modules/map/map.module';
+import {MiscModule} from '@modules/misc/misc.module';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {AppTranslationsLoader} from './app-translations.loader';
+import {AppComponent} from './app.component';
+import {LayoutModule} from './layout/layout.module';
+import {OverlayComponent} from './layout/page/overlay/overlay.component';
+import {UserSettingsComponent} from './layout/page/user-settings/user-settings.component';
+import {ForumTradeModule} from '@modules/forum-trade/forum-trade.module';
 
 const routes: Routes = [
   {
@@ -43,7 +44,8 @@ const routes: Routes = [
     CommandModule,
     MapModule,
     MiscModule,
-    BookmarkModule
+    BookmarkModule,
+    ForumTradeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
