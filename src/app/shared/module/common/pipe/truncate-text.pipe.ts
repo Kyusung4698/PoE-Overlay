98387bf8073ad7603ccbf1b0e60d7bfb2088d894
Nicklas Ronge <nicklas.ronge@gmail.com>
2020-06-24 17:56:30 +0200

    - added trade offer received timer
    - added trade offer received timer
    - added trade player name

--- src/app/shared/module/common/pipe/truncate-text.pipe.ts
+++ src/app/shared/module/common/pipe/truncate-text.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'truncateText',
 })
 export class TruncateTextPipe implements PipeTransform {
-    public transform(text: string, length: number, separator: string = '...'): any {
+    public transform(text: string, length: number, separator: string = '...'): string {
         if (text.length < length) {
             return text;
         }
