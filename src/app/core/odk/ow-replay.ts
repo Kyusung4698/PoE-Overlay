9c4582cf1fdd4a77e44b6ef0aec0bc527fd77b38
Nicklas Ronge <nicklas.ronge@gmail.com>
2020-09-17 18:57:09 +0200

    - added replays location as tooltip

--- src/app/core/odk/ow-replay.ts
+++ src/app/core/odk/ow-replay.ts
@@ -1,6 +1,19 @@
 import { from, Observable } from 'rxjs';
 
 export class OWReplay {
+    public static path(): Observable<string> {
+        const promise = new Promise<string>((resolve, reject) => {
+            overwolf.settings.getOverwolfVideosFolder(result => {
+                if (result.success && result.path?.Value) {
+                    resolve(result.path.Value);
+                } else {
+                    reject(result.error);
+                }
+            });
+        });
+        return from(promise);
+    }
+
     public static turnOn(settings: any): Observable<void> {
         const promise = new Promise<void>((resolve, reject) => {
             overwolf.media.replays.turnOn(settings, result => {
